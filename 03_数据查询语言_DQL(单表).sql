bf43b5990ddd8b004ecc8d6ef1d3470bb6c64a59
ZERO1206 <1562770986@qq.com>
2026-04-28 11:44:54 +0800

    updata

--- 03_数据查询语言_DQL(单表).sql
+++ 03_数据查询语言_DQL(单表).sql
@@ -32,7 +32,7 @@ VALUES
 #select 运算，函数
 SELECT NOW();
 
-#2.指定表查询结果---结果来自指定表格数据
+#2.2 指定表查询结果---结果来自指定表格数据
 /*select 列名,列名,列名... from 表名
  *select 表名.列名,列名... from 表名（用于多表的列名重复）
  *select * | 表名.* from 表名 *->表中所有的列
@@ -55,6 +55,154 @@ SELECT DISTINCT gender FROM t_employee;
 #5.查询常数列
 #select 列,列名,'值' as 列名 from 表名
 SELECT ename NAME,salary, '法拉利' AS company FROM t_employee;
+
+#查询所有员工信息
+SELECT * FROM t_employee;
+
+#查询所有员工信息 并且添加一列etype 值固定为"总部"
+SELECT * , '总部' etype FROM t_employee;
+
+#查询所有员工姓名、工资和工作地址
+SELECT ename , salary , work_place FROM t_employee;
+
+#查询所有员工姓名、薪资和年薪（年薪等于月薪*12）
+SELECT ename , salary , salary*12 AS 年薪 FROM t_employee;
+
+#查询所有员工姓名、月薪、每月奖金、每月总收入
+SELECT ename , salary 月薪, salary * IFNULL(commission_pct, 0) 每月奖金 , salary * (IFNULL(commission_pct, 0) + 1) 每月收入 FROM t_employee;
+#NULL运算 任何值 = NULL; --> ifnull(列, 默认值);
+
+#查询所有员工一共有几种薪资
+SELECT DISTINCT salary 薪资 FROM t_employee;
+
+/*
+2.3 显示表结构---使用命令查看表中的列和列的特征
+	describe 表名
+	desc 表名
+ */
+DESC t_employee;
+
+/*
+2.4 过滤数据（条件查询）
+	select 列 from 表 where [条件 and | or | xor 条件 ...] true | false;
+	from ---> 执行参照和查询表
+	where ---> 进行表中行的数据筛选
+	select ---> 进行符合条件行的数据类的筛选
+ */
+
+#查询工资高于9000的员工信息
+SELECT * FROM t_employee WHERE salary > 9000;
+
+#查询年薪高于100000的员工姓名、薪资和年薪信息
+SELECT ename 姓名 , salary 薪资 , salary*12 年薪 FROM t_employee WHERE salary*12 > 100000;
+
+#查询工资高于8000且性别为女的员工信息
+SELECT * FROM t_employee WHERE salary > 8000 AND gender = '女';
+
+/*
+3.1 算术运算符---对列进行算术运算
+	+ - * /(div) %(MOD)
+	注意
+	 1./浮点除法 div 整数除法 
+	 2./0不会抛出异常 结果是NULL
+	 3.和浮点类型进行运算 结构也是浮点类型
+	 4.优先级和之前一样 提升优先级 使用()
+ */
+
+#查询薪资奖金和大于12000的员工信息
+SELECT * FROM t_employee WHERE salary*(IFNULL(commission_pct, 0) + 1) > 12000;
+
+#查询薪资减去奖金的差小于8000的员工信息
+SELECT * FROM t_employee WHERE salary*(1 - IFNULL(commission_pct, 0)) < 8000;
+
+#查询所有员工的姓名、工资、奖金数额
+SELECT ename 姓名, salary 工资, salary*IFNULL(commission_pct, 0) 奖金数额 FROM t_employee;
+
+#查询员工编号偶数的员工信息
+SELECT * FROM t_employee WHERE eid%2 = 0;
+
+/*
+3.3 比较运算符
+	等于对比
+	=(不能做空判断 null = null -> false   null <=> null ->  true(方言) -> is null) 
+	不等于对比
+	<> 标准语法 !=(mysql方言)
+	大于、小于、大于等于、小于等于 > < >= <=
+	空值处理
+	if null      is not null
+	区间比较
+	between [min] and [max]      not between [min] and [max]
+	范围比较
+	key in(x, y, z) key = x or key = y or key = z	not in(...) ...
+	模糊匹配
+	like | not like -> key like '?%'  key like '_?%'
+ */
+
+#查询员工编号为1的员工信息
+SELECT * FROM t_employee WHERE eid = 1;  # -> SELECT * FROM t_employee WHERE eid = '1'; 
+
+#查询薪资大于5000的员工信息
+SELECT * FROM t_employee WHERE salary > 5000;
+
+#查询有奖金的员工信息
+SELECT * FROM t_employee WHERE commission_pct IS NULL;  # -> SELECT * FROM t_employee WHERE commission_pct <=> NULL;
+
+#查询出生日期在'1990-01-01'和'1995-05-01'之间的员工信息
+SELECT * FROM t_employee WHERE birthday BETWEEN '1990-01-01' AND '1995-05-01';
+
+#查询性别为女性的员工 
+SELECT * FROM t_employee WHERE gender = '女';
+
+#查询手机号码以'138'开头的员工信息
+SELECT * FROM t_employee WHERE tel LIKE '138%';
+
+#查询邮箱以'@abc.com'结尾的员工信息 
+SELECT * FROM t_employee WHERE email LIKE '%@abc.com';
+
+#查询工作地点为'北京','上海','深圳'的员工信息
+SELECT * FROM t_employee WHERE work_place IN ('北京', '上海', '深圳');
+
+/*
+3.5 逻辑运算符
+	and && - or || - not ! - xor
+ */
+
+#查询薪资大于5000且工作地点为'北京'的员工信息
+SELECT * FROM t_employee WHERE salary > 5000 AND work_place LIKE '%北京%';
+#find_in_set ('值', 列名) -> 值是否出现 --- 出现 1 不出现 0
+SELECT ename, FIND_IN_SET('北京', work_place) FROM t_employee;
+SELECT * FROM t_employee WHERE salary > 5000 AND FIND_IN_SET('北京', work_place) > 0;
+
+#查询奖金比例为NULL或者地址为NULL的员工信息
+SELECT * FROM t_employee WHERE commission_pct IS NULL OR address IS NULL;
+
+#查询出生日期在'1995-01-01'之前或者薪资小于4000的员工信息
+SELECT * FROM t_employee WHERE birthday < '1995-01-01' OR salary < 4000;
+
+#查询性别为男且工作地点不为'上海'的员工信息
+SELECT * FROM t_employee WHERE gender = '男' AND FIND_IN_SET('上海', work_place) = 0;
+
+#查询薪资大于5000且工作地点为'北京'或者'上海'的员工信息
+SELECT * FROM t_employee WHERE salary > 5000 AND (FIND_IN_SET('北京', work_place) > 0 OR FIND_IN_SET('上海', work_place) > 0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
